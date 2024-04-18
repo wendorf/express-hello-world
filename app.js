6378dcd7bfc898fd94c7df63de2f93198339fd44
Dan Wendorf <github@danwendorf.com>
2024-04-18 09:30:09 -0700

    Zeus is Home
    Home is good

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const html = `
 <!DOCTYPE html>
 <html>
   <head>
-    <title>Hello from Render!</title>
+    <title>Hello from Zeus, Your Home On The Internet!</title>
     <script src="https://cdn.jsdelivr.net/npm/canvas-confetti@1.5.1/dist/confetti.browser.min.js"></script>
     <script>
       setTimeout(() => {
@@ -52,7 +52,7 @@ const html = `
   </head>
   <body>
     <section>
-      Hello from Render!
+      Hello from Zeus, Your Home On The Internet!
     </section>
   </body>
 </html>
