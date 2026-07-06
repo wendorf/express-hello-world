9275f11cb2dd306ee39e1c3c4c8145b6553a88b2
Dan Wendorf <github@danwendorf.com>
2026-07-06 12:55:23 -0700

    Update app.js
    C

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ if (titleIndex !== -1 && args[titleIndex + 1]) {
     title = args[titleIndex + 1];
 }
 const subtitle = process.env.SUBTITLE || 'Default Subtitle';
-const subtitleTwo = process.env.SUBTITLE_TWO || 'Default Subtitle Two: B';
+const subtitleTwo = process.env.SUBTITLE_TWO || 'Default Subtitle Two: C';
 
 const html = `
 <!DOCTYPE html>
