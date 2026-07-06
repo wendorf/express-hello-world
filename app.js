e63d5a46e8f316854d557b1b8dd11fd0fabb2c67
Dan Wendorf <github@danwendorf.com>
2026-07-06 12:34:47 -0700

    Update app.js
    Update the subtitle

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ if (titleIndex !== -1 && args[titleIndex + 1]) {
     title = args[titleIndex + 1];
 }
 const subtitle = process.env.SUBTITLE || 'Default Subtitle';
-const subtitleTwo = process.env.SUBTITLE_TWO || 'Default Subtitle Two';
+const subtitleTwo = process.env.SUBTITLE_TWO || 'Default Subtitle Two: An Update';
 
 const html = `
 <!DOCTYPE html>
